7449816ced6d2af08a1aedf3b27f6345e68657f1
Ashley-Ann Ellison <aashleyellison51@gmail.com>
2020-10-10 12:54:39 -0500

    Completed exercise 2

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,13 +1,38 @@
 document.addEventListener('DOMContentLoaded', () =>{
     var schild = document.getElementById("board").children;
-    //var i = 0;
+
     var i;
     for(let i = 0; i < schild.length; i++) {
         const squares = schild[i].setAttribute("class", "square");
+        schild[i].addEventListener('click', function() {
+            //console.log('Square' + i + "clicked");
+            schild[i].innerHTML = currentPlayer;
+            changePlayer();
+        });
     }
 
-    //while (i < schild.length) {
-    //    const squares = document.getElementById("board").classList.add("square");
-    //    i++;
-    //}
+    //Stores the letter play
+    let currentPlayer = "X";
+
+    const statusDisplay = document.querySelector('#status');
+
+    statusDisplay.innerHTML = currentPlayerTurn();
+
+    //Tracks the plays
+    let xo = ["", "", "", "", "", "", "", "", ""];
+
+    //Stores the player turn
+    var player_one = 1;
+
+    //Switch players
+    function changePlayer() {
+        if(player_one === 1) {
+            currentPlayer = "X";
+            player_one = 0;
+        } else {
+            currentPlayer = "O";
+            player_one = 1;
+        }
+        
+    }
 });
