0b624a6c3a09f9651e10d95e6455c19b1b8689ab
Ashley-Ann Ellison <aashleyellison51@gmail.com>
2020-10-10 15:16:51 -0500

    Completed exercise 3

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,9 +1,11 @@
 document.addEventListener('DOMContentLoaded', () =>{
-    var schild = document.getElementById("board").children;
 
+    //Carries out main functions
+    var schild = document.getElementById("board").children;
     var i;
     for(let i = 0; i < schild.length; i++) {
         const squares = schild[i].setAttribute("class", "square");
+        highlightSq();
         schild[i].addEventListener('click', function() {
             //console.log('Square' + i + "clicked");
             schild[i].innerHTML = currentPlayer;
@@ -11,12 +13,21 @@ document.addEventListener('DOMContentLoaded', () =>{
         });
     }
 
+    //Changes the colour of the square
+    function highlightSq() {
+        let squares = document.querySelectorAll('.square');
+        squares.forEach(function(highlight) {
+        highlight.addEventListener('mouseover', function(e) {
+            e.target.classList.add('hover');
+        });
+        highlight.addEventListener('mouseout', function(e) {
+            e.target.classList.remove('hover');
+        });
+    });
+    }
+
     //Stores the letter play
     let currentPlayer = "X";
-
-    const statusDisplay = document.querySelector('#status');
-
-    statusDisplay.innerHTML = currentPlayerTurn();
 
     //Tracks the plays
     let xo = ["", "", "", "", "", "", "", "", ""];
@@ -27,12 +38,11 @@ document.addEventListener('DOMContentLoaded', () =>{
     //Switch players
     function changePlayer() {
         if(player_one === 1) {
-            currentPlayer = "X";
+            currentPlayer = "O";
             player_one = 0;
         } else {
-            currentPlayer = "O";
+            currentPlayer = "X";
             player_one = 1;
-        }
-        
+        } 
     }
 });
